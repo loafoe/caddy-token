3e012c6fd153994ef3861570f213c603cfeb5456
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2024-07-13 20:58:20 +0200

    Implement token generator

--- cmd/tokengen/generate.go
+++ cmd/tokengen/generate.go
@@ -5,8 +5,12 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"github.com/loafoe/caddy-token"
 	"math/big"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -21,8 +25,39 @@ var generateCmd = &cobra.Command{
 }
 
 func tokenGenerate(cmd *cobra.Command, args []string) {
-	randomString := generateRandomString(24)
-	fmt.Println("generate called", randomString)
+	org, _ := cmd.Flags().GetString("organization")
+	env, _ := cmd.Flags().GetString("environment")
+	region, _ := cmd.Flags().GetString("region")
+	project, _ := cmd.Flags().GetString("project")
+	if org == "" || env == "" || region == "" || project == "" {
+		fmt.Println("Please provide all required parameters")
+		return
+	}
+	randomCount := 24
+	bail := 3
+	for {
+		if bail <= 0 {
+			fmt.Println("Failed to generate a valid token")
+			return
+		}
+		randomString := generateRandomString(randomCount)
+		var newToken token.Key
+		newToken.Organization = org
+		newToken.Environment = env
+		newToken.Region = region
+		newToken.Project = project
+		newToken.Token = randomString
+		marshalled, _ := json.Marshal(newToken)
+		key := base64.StdEncoding.EncodeToString(marshalled)
+		if strings.HasSuffix(key, "=") {
+			randomCount = randomCount + 1
+			bail = bail - 1
+			continue
+		}
+		// We have a good-looking newToken, return it
+		fmt.Printf("%s%s\n", token.Prefix, key)
+		return
+	}
 }
 
 // generateRandomString generates a random alphanumeric string of length n.
